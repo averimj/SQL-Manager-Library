f80a15b7e7db92463ad61079921e45efacbd8273
Averi Johnson <averimj@live.com>
2020-12-02 23:48:45 -0700

    WIP routes

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const { sequelize, Book } = require('../models');
+
+// not sure if I need both nor which syntax is most up-to-date AND I have in index.js
+// const { sequelize, Book } = require('../models');
+const Book = require('../models').Book;
+
+// Handler function to wrap each route
+function asyncHandler(cb){
+  return async(req,res,next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // forward error to the global error Handler
+      next(error);
+    }
+  }
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler(async(req, res, next) => {
+  const books = await Book.findAll();
   res.send('respond with a resource');
-});
+}));
+
+// error message WIP //
+// router.get('/error', (reg, res, next) => {
+//   console.log('Custom error route called');
+//
+//   const err = new Error();
+//   err.message = 'Custom 500 error thrown'
+//   err.status = 500;
+//   throw err;
+// });
+
 
 module.exports = router;
