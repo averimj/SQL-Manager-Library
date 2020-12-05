19b55ed892e9ffe36c1880b29967757bec72f686
Averi Johnson <averimj@live.com>
2020-12-05 01:11:56 -0700

    clean up routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const Book = require('../models').Book;
 
 /* GET home page. */
-// router.get('/', async(req, res, next) => {
-//   const books = await Book.findAll();
-//   res.json(books);
-// });
-
 router.get('/', (req, res, next) => {
   res.redirect('/books');
 });
