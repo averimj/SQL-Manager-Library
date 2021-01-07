c2b308068f75ba040bdf64d72f5c3eb8fd0ff67c
Averi Johnson <averimj@live.com>
2021-01-06 20:21:20 -0700

    change error back to err

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
 
   // got from error handle video
-  if (error.status === 404) {
+  if (err.status === 404) {
     res.status(404).render('page-not-found', { error });
   } else {
     err.message = err.message || 'Houston, we have a problem with the server!'
