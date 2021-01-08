0c3c735a22ed4e2d08ab20cde42123ff079922d6
Averi Johnson <averimj@live.com>
2021-01-07 23:58:34 -0700

    fix get/:id route

--- routes/books.js
+++ routes/books.js
@@ -43,13 +43,15 @@ router.post('/new', asyncHandler(async (req, res) => {
 }));
 
 /* UPDATE a single book */
-router.get('/:id', asyncHandler(async(req, res) => {
+router.get('/:id', asyncHandler(async(req, res, next) => {
   const book = await Book.findByPk(req.params.id);
-  if (book) {
-    res.render('update_book', { book, title: book.title})
-  } else {
-    res.status(404).render('page-not-found');
-  }
+    if(book) {
+      res.render('update_book', { book, title: book.title})
+    } else {
+      const error = new Error();
+      error.status = 404;
+      throw error;
+    }
 }));
 
 /* POST an updated book. */
